14d115c19f7e1c24ea868fef9e042f2e8ebc7c0f
Srikar746 <srikarchandra16319@gmail.com>
2025-07-30 16:10:18 +0530

    commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello.iam srikar"
+    return "Hello.iam srikar Chandra"
 
 if __name__ == "__main__":
     app.run("0.0.0.0",port=5000)
